1281e4be7b6dc506e90c8b272b82d23e025f739f
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-05-03 23:39:48 +0330

    testing Router

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <Label />
+      <Label labelId="welcome" />
     </div>
   );
 };
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,23 @@
 import "@testing-library/jest-dom/vitest";
+import { PropsWithChildren, ReactNode } from "react";
 import ResizeObserver from "resize-observer-polyfill";
 import { server } from "./mocks/server";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+vi.mock("@auth0/auth0-react", () => {
+  return {
+    useAuth0: vi.fn().mockReturnValue({
+      isAuthenticated: false,
+      isLoading: false,
+      user: undefined,
+    }),
+    Auth0Provider: ({ children }: PropsWithChildren) => children,
+    withAuthenticationRequired: (component: ReactNode) => component,
+  };
+});
 
 global.ResizeObserver = ResizeObserver;
 
@@ -16,6 +29,7 @@ Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: vi.fn().mockImplementation((query) => ({
     matches: false,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     media: query,
     onchange: null,
     addListener: vi.fn(), // deprecated
